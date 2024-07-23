c513c17de3ff7eda2b1e4a0cf6fbeda3e75875ed
bennett <licht131@naver.com>
2024-07-23 23:46:15 +0900

    solve : [PRO] 미로 탈출_240721
    https://school.programmers.co.kr/learn/courses/30/lessons/159993

--- Week04/Problem03.java
+++ Week04/Problem03.java
@@ -22,7 +22,7 @@ class Solution {
         setLocation();
 
         // start에서 lever로 이동하는 경로를 탐색한다.
-        // 경로가 없다면 탐색을 중단한다.
+        // 경로가 없다면 -1을 반환한다.
       
         int d1 = bfs(start, lever);
         if(d1 == -1) return -1;
@@ -33,7 +33,7 @@ class Solution {
         int d2 = bfs(lever, end);
         if(d2 == -1) return -1;
 
-        // 두 경로의 길이를 합산힌디.
+        // 두 경로의 길이를 합산하여 반환한다.
       
         return d1 + d2;
     }
@@ -59,19 +59,29 @@ class Solution {
     
     
     static int bfs(int[] source, int[] destination) {
+        
         // {현재 행, 현재 열, 현재 위치까지의 거리} 형태로 큐에 저장한다.
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] {source[0], source[1], 0});
+
+        // 방문 배열 초기화하고, 출발지를 방문했다고 표시함.
         
         visited = new boolean[row][col];
         visited[source[0]][source[1]] = true;
         
         while(!queue.isEmpty()) {
             int[] loc = queue.poll();
+
+            // 목적지에 도달하면 현재 위치까지의 거리(최단 경로)를 반환한다.
             
             if(loc[0] == destination[0] && loc[1] == destination[1]) {
                 return loc[2];
             }
+
+            // 방향별로 한 칸씩 이동해본다.
+            // 올바른 위치인지 확인하고, 아직 방문하지 않은 통로인지 확인한다.
+            // 조건을 만족하면 큐와 방문 배열을 업데이트한다.
             
             for(int i=0; i<4; i++) {
                 int x = loc[0] + dx[i];
@@ -84,6 +94,8 @@ class Solution {
                 }
             }
         }
+
+        // bfs를 진행하는 동안 목적지에 도달하지 못한다면 -1을 반환한다.
         
         return -1;
     }
